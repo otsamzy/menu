587acee42c7650c2df71f2a6aab448c70fdd6a75
otsamzy <109823511+otsamzy@users.noreply.github.com>
2023-10-16 19:15:02 +0100

    Update Nav.js

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 const Nav = ({ category, selectBtn }) => {
+const [currNumber, setCurrNumber] = useState(0); 
   return (
     <nav className="nav_container">
       <ul className="nav">
         {category.map((cate, index) => {
           return (
-            <li
+            <li  
+             className={`${
+                index === currNumber ? "category active" : "category"
+              } `}
               key={index}
               className={"category"}
               onClick={() => selectBtn(cate)}
